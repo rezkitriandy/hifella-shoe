9b8c30aa31573e98a75b121735755aa765cb477b
Rezki Triandy <rezki.hifella@gmail.com>
2025-11-12 15:44:14 +0700

    chore: folder restructure

--- src/shared/providers/ReactQueryProvider.tsx
+++ src/shared/providers/ReactQueryProvider.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import queryClient from '@/lib/queryClient';
 import { QueryClientProvider } from '@tanstack/react-query';
+import queryClient from '../lib/queryClient';
 
 export default function ReactQueryProvider({
   children,
